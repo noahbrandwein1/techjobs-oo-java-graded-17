e980c1fc2639cae4a3e2d6e3f6a508138994aae6
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-10-05 19:52:30 -0500

    Fixing Tests

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,53 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+public class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public JobField (String value) {
+        this();
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField jobField)) return false;
+        return getId() == jobField.getId() && Objects.equals(getValue(), jobField.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getValue());
+    }
+}
